cad8e0032d1b4cf01ff4259621a021d16e6b77a8
Ivan Ilin <vanilin.va@yandex.ru>
2025-08-21 16:14:31 +0300

    add first scene

--- Assets/Scripts/Tank/TankController.cs
+++ Assets/Scripts/Tank/TankController.cs
@@ -119,7 +119,9 @@ namespace Tanks
             _prevPosition = position;
 
             CurrentSpeed = (float)Math.Round((double)distance / Time.deltaTime * c_convertMeterInSecFromKmInH, 1);
-            _camera.m_Lens.FieldOfView = Mathf.Lerp(_fov.x, _fov.y, Mathf.InverseLerp(0f, _maxSpeedFOV, CurrentSpeed));
+            // TODO: разобраться с cinemachine
+            //Debug.Log(_camera.m_Lens);
+            //_camera.m_Lens.FieldOfView = Mathf.Lerp(_fov.x, _fov.y, Mathf.InverseLerp(0f, _maxSpeedFOV, CurrentSpeed));
         }
 
         private void ApplyDrive()
