94beceb287abc1743b31d810fe62e81d7001e6ef
Ivan Ilin <vanilin.va@yandex.ru>
2025-12-09 22:16:30 +0300

    start homework

---
+++ Assets/Scripts/EditorMover.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	
+	[RequireComponent(typeof(PositionSaver))]
+	public class EditorMover : MonoBehaviour
+	{
+        [SerializeField]
+        private PositionSaver _save;
+        [SerializeField]
+        private float _currentDelay;
+
+		//todo comment: Что произойдёт, если _delay > _duration?
+		[SerializeField, Range(0.2f, 1.0f)]
+		private float _delay = 0.5f;
+		[SerializeField, Min(0.2f)]
+		private float _duration = 5f;
+
+		private void Start()
+		{
+			//не увидел 
+			if (_duration < _delay) _duration = _delay * 5;
+			//todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+			//хуки. вспомнить что такое start, что такое update
+			_save = GetComponent<PositionSaver>();
+			_save.Records.Clear();
+		}
+
+		private void Update()
+		{
+			_duration -= Time.deltaTime;
+			if (_duration <= 0f)
+			{
+				enabled = false;
+				Debug.Log($"<b>{name}</b> finished", this);
+				return;
+			}
+			
+			//todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+			_currentDelay -= Time.deltaTime;
+			if (_currentDelay <= 0f)
+			{
+				_currentDelay = _delay;
+				_save.Records.Add(new PositionSaver.Data
+				{
+					Position = transform.position,
+					//todo comment: Для чего сохраняется значение игрового времени?
+					Time = Time.time,
+				});
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PositionSaver.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class PositionSaver : MonoBehaviour
+	{
+		[SerializeField]
+		public struct Data
+		{
+			public Vector3 Position;
+			public float Time;
+		}
+
+        [SerializeField]
+        private TextAsset _json;
+
+        [SerializeField]
+        public List<Data> Records { get; private set; }
+
+		private void Awake()
+		{
+			//todo comment: Что будет, если в теле этого условия не сделать выход из метода?
+			//продолжится выполнение метода. на первой же инструкции выпадет ошибка, так как будет попытка обратится к свойству пустого объекта
+			if (_json == null)
+			{
+				gameObject.SetActive(false);
+				Debug.LogError("Please, create TextAsset and add in field _json");
+				return;
+			}
+			
+			JsonUtility.FromJsonOverwrite(_json.text, this);
+			//todo comment: Для чего нужна эта проверка (что она позволяет избежать)?
+			//если массив Records пустой, то инициализирует его пустыми объектами
+			if (Records == null)
+				Records = new List<Data>(10);
+		}
+
+		private void OnDrawGizmos()
+		{
+			//todo comment: Зачем нужны эти проверки (что они позволляют избежать)?
+			//предупреждает работу с пустым массивом. При инициализации переменной prev идет обращение к первому элементу массива,
+			//если массив пустой или не инициализирован, то будет ошибка
+			if (Records == null || Records.Count == 0) return;
+			var data = Records;
+			var prev = data[0].Position;
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(prev, 0.3f);
+			//todo comment: Почему итерация начинается не с нулевого элемента?
+			//в 0 позиции мы уже находимся, нам надо обработать следующее положение кубика
+			for (int i = 1; i < data.Count; i++)
+			{
+				var curr = data[i].Position;
+				Gizmos.DrawWireSphere(curr, 0.3f);
+				Gizmos.DrawLine(prev, curr);
+				prev = curr;
+			}
+		}
+		
+#if UNITY_EDITOR
+		[ContextMenu("Create File")]
+		private void CreateFile()
+		{
+			//todo comment: Что происходит в этой строке?
+			var stream = File.Create(Path.Combine(Application.dataPath, "Path.txt"));
+			//todo comment: Подумайте для чего нужна эта строка? (а потом проверьте догадку, закомментировав) 
+			stream.Dispose();
+			UnityEditor.AssetDatabase.Refresh();
+			//В Unity можно искать объекты по их типу, для этого используется префикс "t:"
+			//После нахождения, Юнити возвращает массив гуидов (которые в мета-файлах задаются, например)
+			var guids = UnityEditor.AssetDatabase.FindAssets("t:TextAsset");
+			foreach (var guid in guids)
+			{
+				//Этой командой можно получить путь к ассету через его гуид
+				var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+				//Этой командой можно загрузить сам ассет
+				var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+				//todo comment: Для чего нужны эти проверки?
+				if(asset != null && asset.name == "Path")
+				{
+					_json = asset;
+					UnityEditor.EditorUtility.SetDirty(this);
+					UnityEditor.AssetDatabase.SaveAssets();
+					UnityEditor.AssetDatabase.Refresh();
+					//todo comment: Почему мы здесь выходим, а не продолжаем итерироваться?
+					return;
+				}
+			}
+		}
+
+		private void OnDestroy()
+		{
+			//todo logic...
+		}
+#endif
+	}
+}
