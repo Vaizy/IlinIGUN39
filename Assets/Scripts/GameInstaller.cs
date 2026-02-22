03d03641faaa65612a1e1044df1a0062e6f1a1cf
Ivan Ilin <vanilin.va@yandex.ru>
2026-02-22 18:13:17 +0300

    собран уровень

--- Assets/Scripts/GameInstaller.cs
+++ Assets/Scripts/GameInstaller.cs
@@ -3,7 +3,13 @@ using Zenject;
 
 public class GameInstaller : MonoInstaller
 {
+    private Controls _controls;
+
+    [SerializeField]
+    private SceneController _controller;
     public override void InstallBindings()
     {
+        _controls = new Controls();
+        _controls.Game.Enable();
     }
 }
